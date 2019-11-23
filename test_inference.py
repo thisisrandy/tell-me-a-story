97fef2f076d40395c3ae1f5ec0b4b67b4999d1be
Randy West <randywest55@gmail.com>
2019-11-23 14:03:04 -0500

    Use wrap_print

--- test_inference.py
+++ test_inference.py
@@ -1,11 +1,12 @@
 from modelinference import ModelInference
 from random import randint
-from textcleaner import clean_children_gpt2 as clean
+from textcleaner import wrap_print, clean_children_gpt2 as clean
 
 length = 100
 seed = randint(1, 50000)
 prompt = "Once upon a time, Jack found a magical bean."
 inference = ModelInference("gpt2", "./models/fine-tuned_gpt2_1000/", seed=seed)
 for response in inference.sample_and_decode(prompt, length):
-    print(f"== PROMPT ==\n\n{prompt}\n\n== RESPONSE ==\n{clean(response)}")
+    print(f"== PROMPT ==\n\n{prompt}\n\n== RESPONSE ==\n")
+    wrap_print(clean(response))
 
