7411f16007c624aafb18b23f575275102fb86f68
Randy West <randywest55@gmail.com>
2019-11-23 14:05:02 -0500

    Make printing a little prettier

--- test_inference.py
+++ test_inference.py
@@ -7,6 +7,6 @@ seed = randint(1, 50000)
 prompt = "Once upon a time, Jack found a magical bean."
 inference = ModelInference("gpt2", "./models/fine-tuned_gpt2_1000/", seed=seed)
 for response in inference.sample_and_decode(prompt, length):
-    print(f"== PROMPT ==\n\n{prompt}\n\n== RESPONSE ==\n")
+    print(f"== PROMPT ==\n\n{prompt}\n\n== RESPONSE ==")
     wrap_print(clean(response))
 
