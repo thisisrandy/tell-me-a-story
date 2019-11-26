4ad50a1c9433818d7d45279729e58b33e9da397b
Randy West <randywest55@gmail.com>
2019-11-26 12:31:34 -0500

    Minimal working first version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,135 @@
-import React from 'react';
-import logo from './peter_rabbit.jpg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  TextField,
+  CssBaseline,
+  Button,
+  Paper,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Menu } from "@material-ui/icons";
+
+// const apiUrl = "https://story-teller-3vkz2hdbua-ue.a.run.app/story?";
+const apiUrl = "http://0.0.0.0:8008/story?";
+const storyLength = 50;
+
+const useStyles = makeStyles(theme => ({
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  paper: {
+    margin: 20,
+    padding: 20,
+    maxWidth: 800
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const [prompt, setPrompt] = useState("");
+  const [promptDisabled, setPromptDisabled] = useState(false);
+  const handlePromptUpdate = ({ target }) => setPrompt(target.value);
+  const [stories, setStories] = useState([]);
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (prompt) {
+      setPromptDisabled(true);
+      const id = Date.now();
+      setStories([
+        ...stories,
+        {
+          id: id,
+          prompt: prompt,
+          story: "Composing... (this will take a minute or two)"
+        }
+      ]);
+      const url = `${apiUrl}length=${storyLength}&prompt=${encodeURI(prompt)}`;
+      setPrompt("");
+      const res = await fetch(url);
+      const json = await res.json();
+      setStories([
+        ...stories.slice(0, stories.length - 1),
+        {
+          id: id,
+          prompt: json.prompt,
+          story: json.story
+        }
+      ]);
+      setPromptDisabled(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Tell Me a Story
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.content}>
+        <Paper className={classes.paper}>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              name="prompt"
+              label={
+                promptDisabled
+                  ? "Please wait for the computer to finish the story"
+                  : "Story prompt"
+              }
+              value={prompt}
+              onChange={handlePromptUpdate}
+              autoFocus={true}
+              margin="normal"
+              helperText={
+                'Try "Once upon a time." The computer will tell a different story every time!'
+              }
+              disabled={promptDisabled}
+            />
+            <Button type="submit" color="primary" variant="contained">
+              Generate Story
+            </Button>
+          </form>
+        </Paper>
+        <List>
+          {stories
+            .slice(0)
+            .reverse()
+            .map(({ id, prompt, story }) => (
+              <ListItem key={id}>
+                <ListItemText primary={prompt} secondary={story} />
+              </ListItem>
+            ))}
+        </List>
+      </div>
+    </React.Fragment>
   );
 }
 
