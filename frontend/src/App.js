925359dc9f06f667cd58a05e0c96b42eb4e8de0e
Randy West <randywest55@gmail.com>
2019-11-26 16:41:48 -0500

    Use HideOnScroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Menu } from "@material-ui/icons";
+import HideOnScroll from "./HideOnScroll";
 import PeterRabbit from "./peter_rabbit.jpg";
 
 // const apiUrl = "https://story-teller-3vkz2hdbua-ue.a.run.app/story?";
@@ -92,21 +93,23 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <Menu />
-          </IconButton>
-          <Typography variant="h3" className={classes.title}>
-            Tell Me a Story
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <HideOnScroll>
+        <AppBar position="sticky">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <Menu />
+            </IconButton>
+            <Typography variant="h3" className={classes.title}>
+              Tell Me a Story
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       <div className={classes.content}>
         <img src={PeterRabbit} alt="Peter Rabbit" />
         <Paper className={classes.paper}>
