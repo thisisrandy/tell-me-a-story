957589211ab35faa4a75f01eb1bd809983e9dcae
Randy West <randywest55@gmail.com>
2019-11-27 06:07:22 -0500

    Add simple top menu

--- frontend/src/TopBarContainer.js
+++ frontend/src/TopBarContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { Typography, AppBar, Toolbar, IconButton } from "@material-ui/core";
-import { Menu } from "@material-ui/icons";
+import React, { useState } from "react";
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import HideOnScroll from "./HideOnScroll";
 import { useStyles } from "./useStyles";
 
 export default function TopBarContainer() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <HideOnScroll>
@@ -16,9 +32,22 @@ export default function TopBarContainer() {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            aria-controls="main-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
           >
-            <Menu />
+            <MenuIcon />
           </IconButton>
+          <Menu
+            id="main-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>What&apos;s this?</MenuItem>
+            <MenuItem onClick={handleClose}>Show me the code!</MenuItem>
+          </Menu>
           <Typography variant="h3" className={classes.title}>
             Tell Me a Story
           </Typography>
