694c9dede7b40534b9947c37134ad07031664fc6
Randy West <randywest55@gmail.com>
2019-11-27 05:01:05 -0500

    Remove reference to index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import { brown } from "@material-ui/core/colors";
