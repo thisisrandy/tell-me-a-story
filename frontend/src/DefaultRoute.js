993f64954c24e9c67818beb05125563a12025118
Randy West <randywest55@gmail.com>
2019-11-28 15:38:56 -0500

    Wrap DefaultRoute in Paper

--- frontend/src/DefaultRoute.js
+++ frontend/src/DefaultRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Paper } from "@material-ui/core";
 import PeterRabbit from "./peter_rabbit.jpg";
 import { useStyles } from "./useStyles";
 import ButtonLink from "./ButtonLink";
@@ -10,12 +10,14 @@ export default function DefaultRoute() {
   return (
     <div className={classes.content}>
       <img src={PeterRabbit} alt="Peter Rabbit" />
-      <Typography variant="h5" className={classes.defaultRoute}>
-        Nothing here (yet?)!
-      </Typography>
-      <ButtonLink to="/stories" className={classes.defaultRoute}>
-        Return home
-      </ButtonLink>
+      <Paper className={classes.paper}>
+        <Typography variant="h5" className={classes.defaultRoute}>
+          Nothing here (yet?)!
+        </Typography>
+        <ButtonLink to="/stories" className={classes.defaultRoute}>
+          Return home
+        </ButtonLink>
+      </Paper>
     </div>
   );
 }
