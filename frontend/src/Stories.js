92472c806ca8b31a35b03eb5c3f2093d3f955256
Randy West <randywest55@gmail.com>
2019-11-30 12:12:40 -0500

    Refactor StoryForm out of Stories

--- frontend/src/Stories.js
+++ frontend/src/Stories.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  TextField,
-  Button,
-  Paper,
   Card,
   CardActions,
   CardContent,
   Typography,
   CircularProgress,
   IconButton,
-  Tooltip,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions
+  Tooltip
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ClipboardIcon from "@material-ui/icons/Assignment";
@@ -23,58 +15,15 @@ import { useStateWithLocalStorage } from "./useStateWithLocalStorage";
 import clsx from "clsx";
 import * as clipboard from "clipboard-polyfill/dist/clipboard-polyfill.promise";
 import dateFormat from "dateformat";
-
-// const apiUrl = "https://story-teller-3vkz2hdbua-ue.a.run.app/story?quote_style=cursive&";
-// const storyLength = 500;
-const apiUrl = "http://0.0.0.0:8008/story?quote_style=cursive&";
-const storyLength = 50;
+import StoryForm from "./StoryForm";
 
 export default function Stories() {
   const classes = useStyles();
-  const [prompt, setPrompt] = useState("");
-  const [promptDisabled, setPromptDisabled] = useState(false);
-  const handlePromptUpdate = ({ target }) => setPrompt(target.value);
   const [stories, setStories] = useStateWithLocalStorage(
     "stories",
     [],
     story => !story.isGenerating
   );
-  const [clearAllDialogOpen, setClearAllDialogOpen] = useState(false);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    if (prompt) {
-      setPrompt("");
-      setPromptDisabled(true);
-      const id = Date.now();
-      // note that it's still safe to use prompt because it remains stale
-      // inside the closure
-      setStories([
-        ...stories,
-        {
-          id: id,
-          prompt: prompt,
-          isGenerating: true
-        }
-      ]);
-      const res = await fetch(
-        `${apiUrl}length=${storyLength}&prompt=${encodeURI(prompt)}`
-      );
-      const json = await res.json();
-      // stories is stale inside this closure. we can get the fresh value by
-      // using a functional update
-      setStories(stories => [
-        ...stories.slice(0, stories.length - 1),
-        {
-          id: id,
-          prompt: json.prompt,
-          story: json.story,
-          isGenerating: false
-        }
-      ]);
-      setPromptDisabled(false);
-    }
-  };
 
   const handleCopy = id => () => {
     const toCopy = stories.filter(s => s.id === id)[0];
@@ -87,73 +36,7 @@ export default function Stories() {
 
   return (
     <div className={classes.content}>
-      <Paper className={classes.paper}>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <TextField
-            name="prompt"
-            className={classes.formElement}
-            label={promptDisabled ? "Composing..." : "Story prompt"}
-            value={prompt}
-            onChange={handlePromptUpdate}
-            autoFocus={true}
-            margin="normal"
-            helperText={
-              'Try "Once upon a time." The computer will tell a different story every time!'
-            }
-            disabled={promptDisabled}
-          />
-          <span className={clsx(classes.formElement, classes.buttonsContainer)}>
-            <Tooltip title="Generate a new story using the prompt you typed">
-              <Button
-                type="submit"
-                color="primary"
-                variant="contained"
-                disabled={promptDisabled}
-              >
-                Generate Story
-              </Button>
-            </Tooltip>
-            <Tooltip title="Delete all generated stories">
-              <Button
-                color="primary"
-                variant="contained"
-                disabled={promptDisabled || stories.length === 0}
-                onClick={() => setClearAllDialogOpen(true)}
-              >
-                Clear All
-              </Button>
-            </Tooltip>
-            <Dialog
-              open={clearAllDialogOpen}
-              onClose={() => setClearAllDialogOpen(false)}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">
-                {"Are you sure?"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  All generated stories will be permanently deleted
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={() => setClearAllDialogOpen(false)}>
-                  Cancel
-                </Button>
-                <Button
-                  onClick={() => {
-                    setStories([]);
-                    setClearAllDialogOpen(false);
-                  }}
-                >
-                  Proceed
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </span>
-        </form>
-      </Paper>
+      <StoryForm stories={stories} setStories={setStories} />
       {stories
         .slice(0)
         .reverse()
---
+++ frontend/src/StoryForm.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  TextField,
+  Button,
+  Paper,
+  Tooltip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from "@material-ui/core";
+import clsx from "clsx";
+import { useStyles } from "./useStyles";
+
+// const apiUrl = "https://story-teller-3vkz2hdbua-ue.a.run.app/story?quote_style=cursive&";
+// const storyLength = 500;
+const apiUrl = "http://0.0.0.0:8008/story?quote_style=cursive&";
+const storyLength = 50;
+
+export default function StoryForm({ stories, setStories }) {
+  const classes = useStyles();
+  const [prompt, setPrompt] = useState("");
+  const [promptDisabled, setPromptDisabled] = useState(false);
+  const handlePromptUpdate = ({ target }) => setPrompt(target.value);
+  const [clearAllDialogOpen, setClearAllDialogOpen] = useState(false);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (prompt) {
+      setPrompt("");
+      setPromptDisabled(true);
+      const id = Date.now();
+      // note that it's still safe to use prompt because it remains stale
+      // inside the closure
+      setStories([
+        ...stories,
+        {
+          id: id,
+          prompt: prompt,
+          isGenerating: true
+        }
+      ]);
+      const res = await fetch(
+        `${apiUrl}length=${storyLength}&prompt=${encodeURI(prompt)}`
+      );
+      const json = await res.json();
+      // stories is stale inside this closure. we can get the fresh value by
+      // using a functional update
+      setStories(stories => [
+        ...stories.slice(0, stories.length - 1),
+        {
+          id: id,
+          prompt: json.prompt,
+          story: json.story,
+          isGenerating: false
+        }
+      ]);
+      setPromptDisabled(false);
+    }
+  };
+
+  return (
+    <Paper className={classes.paper}>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          name="prompt"
+          className={classes.formElement}
+          label={promptDisabled ? "Composing..." : "Story prompt"}
+          value={prompt}
+          onChange={handlePromptUpdate}
+          autoFocus={true}
+          margin="normal"
+          helperText={
+            'Try "Once upon a time." The computer will tell a different story every time!'
+          }
+          disabled={promptDisabled}
+        />
+        <span className={clsx(classes.formElement, classes.buttonsContainer)}>
+          <Tooltip title="Generate a new story using the prompt you typed">
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              disabled={promptDisabled}
+            >
+              Generate Story
+            </Button>
+          </Tooltip>
+          <Tooltip title="Delete all generated stories">
+            <Button
+              color="primary"
+              variant="contained"
+              disabled={promptDisabled || stories.length === 0}
+              onClick={() => setClearAllDialogOpen(true)}
+            >
+              Clear All
+            </Button>
+          </Tooltip>
+          <Dialog
+            open={clearAllDialogOpen}
+            onClose={() => setClearAllDialogOpen(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                All generated stories will be permanently deleted
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setClearAllDialogOpen(false)}>
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {
+                  setStories([]);
+                  setClearAllDialogOpen(false);
+                }}
+              >
+                Proceed
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </span>
+      </form>
+    </Paper>
+  );
+}
+
+StoryForm.propTypes = {
+  stories: PropTypes.array.isRequired,
+  setStories: PropTypes.func.isRequired
+};
